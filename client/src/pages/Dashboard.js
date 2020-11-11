e2e6759c81f7ad5a20a26f8d286e0606951c4b6a
Kennadee <kennadee.ray@gmail.com>
2020-11-11 11:48:36 -0600

    dashboard hardcode

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,67 +8,108 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 // import { ADD_FRIEND } from '../utils/mutations';
 import { useQuery, useMutation } from '@apollo/react-hooks';
+import { Container, Grid, Card, Button } from 'semantic-ui-react';
+
+const items = [
+    {
+        header: 'Should I go to Hawaii?',
+        description:
+            'Here are some pros and cons...',
+        meta: 'Active May 3rd, 2016',
+    },
+    {
+        header: 'Should I change careers?',
+        description:
+            'Some more pros and cons...',
+        meta: 'Active November 9th, 2020',
+    },
+    {
+        header: 'Should I go to Hawaii?',
+        description:
+            'Here are some pros and cons...',
+        meta: 'Active May 3rd, 2016',
+    },
+    {
+        header: 'Should I change careers?',
+        description:
+            'Some more pros and cons...',
+        meta: 'Active November 9th, 2020',
+    },
+    {
+        header: 'Should I go to Hawaii?',
+        description:
+            'Here are some pros and cons...',
+        meta: 'Active May 3rd, 2016',
+    },
+    {
+        header: 'Should I change careers?',
+        description:
+            'Some more pros and cons...',
+        meta: 'Active November 9th, 2020',
+    },
+    {
+        header: 'Should I go to Hawaii?',
+        description:
+            'Here are some pros and cons...',
+        meta: 'Active May 3rd, 2016',
+    },
+    {
+        header: 'Should I change careers?',
+        description:
+            'Some more pros and cons...',
+        meta: 'Active November 9th, 2020',
+    },
+    {
+        header: 'Should I go to Hawaii?',
+        description:
+            'Here are some pros and cons...',
+        meta: 'Active May 3rd, 2016',
+    },
+    {
+        header: 'Should I change careers?',
+        description:
+            'Some more pros and cons...',
+        meta: 'Active November 9th, 2020',
+    }
+]
 
 const Dashboard = () => {
-    const { username: userParam } = useParams();
-    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-        variables: { username: userParam }
-    });
-    const user = data?.me || data?.user || {};
-    // const [addFriend] = useMutation(ADD_FRIEND)
-    if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-        return <Redirect to="/dashboard" />
-    }
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-    if (!user?.username) {
-        return (
-            <h4>
-                You need to be logged in to see this page.
-            </h4>
-        )
-    }
-    const handleClick = async () => {
-        // try {
-        //   await addFriend({
-        //     variables: { id: user._id }
-        //   })
-        // } catch (e) {
-        //   console.error(e)
-        // }
-    }
+    // const { username: userParam } = useParams();
+    // const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    //     variables: { username: userParam }
+    // });
+    // const user = data?.me || data?.user || {};
+    // // const [addFriend] = useMutation(ADD_FRIEND)
+    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    //     return <Redirect to="/dashboard" />
+    // }
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
+    // if (!user?.username) {
+    //     return (
+    //         <h4>
+    //             You need to be logged in to see this page.
+    //         </h4>
+    //     )
+    // }
+    // const handleClick = async () => {
+    //     // try {
+    //     //   await addFriend({
+    //     //     variables: { id: user._id }
+    //     //   })
+    //     // } catch (e) {
+    //     //   console.error(e)
+    //     // }
+    // }
 
     return (
-        <main>
-            <div>
-                <div>
-                    <h2>
-                        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-                    </h2>
-                    {userParam && (
-                        <button onClick={handleClick}>
-                            Add Friend
-                        </button>
-                    )}
-                </div>
+        <Container className="chess" style={{ paddingTop: '30vh', paddingBottom: '29vh' }}>
+            <Button href="/create">Start a new decision here!</Button>
+            <Card.Group centered stackable items={items}>
 
-                <div>
-                    <div>
-                        <DecisionList decisions={user.decisions} title={`${user.username}'s decisions...`} />
-                    </div>
-
-                    {/* <div>
-                        <FriendList
-                            username={user.username}
-                            friendCount={user.friendCount}
-                            friends={user.friends}
-                        />
-                    </div> */}
-                </div>
-            </div>
-            <Link to="/create">Start a new decision here!</Link>
-        </main>
+            </Card.Group>
+        </Container>
     );
 }
 
